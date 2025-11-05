74f309e13fd72d3da86f147c8cbdcb5bce2a5e22
Maaz Ul Haq <maazulhaq00@gmail.com>
2025-11-05 13:02:19 +0500

    react template Category POST API

---
+++ admin-dashboard/src/routes.jsx
@@ -0,0 +1,54 @@
+import { Outlet } from 'react-router-dom';
+import { Layout as DashboardLayout } from './layouts/dashboard/layout';
+import IconsPage from './pages1/icons';
+import NotFoundPage from './pages1/404';
+import OrdersPage from './pages1/orders';
+import ReportsPage from './pages1';
+import SettingsPage from './pages1/settings';
+import ThemePage from './pages1/theme';
+import CreateCategory from './pages/CreateCategory';
+
+export const routes = [
+  {
+    element: (
+      <DashboardLayout>
+        <Outlet />
+      </DashboardLayout>
+    ),
+    children: [
+      {
+        index: true,
+        element: <ReportsPage />
+      },
+      {
+        path: 'add-category',
+        element: <CreateCategory />
+      },
+      //old routes
+      {
+        path: 'orders',
+        element: <OrdersPage />
+      },
+      {
+        path: 'settings',
+        element: <SettingsPage />
+      },
+      {
+        path: 'theme',
+        element: <ThemePage />
+      },
+      {
+        path: 'icons',
+        element: <IconsPage />
+      }
+    ]
+  },
+  {
+    path: '404',
+    element: <NotFoundPage />
+  },
+  {
+    path: '*',
+    element: <NotFoundPage />
+  }
+];
